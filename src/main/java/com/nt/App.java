f8a2561039c55c1c55fb84eba2d560c70072fb6f
dibya <dibya2939@gmail.com>
2026-08-30 22:30:41 +0530

    Initial commit

---
+++ src/main/java/com/nt/App.java
@@ -0,0 +1,10 @@
+package com.nt;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    public static void printSomething() {
+        System.out.println("Print Something");
+    }
+}
