302260838f63eda992b385645414c7f591045b56
dibya <dibya2939@gmail.com>
2026-08-30 22:47:29 +0530

    Changes some feature branch

--- src/main/java/com/nt/App.java
+++ src/main/java/com/nt/App.java
@@ -2,13 +2,13 @@ package com.nt;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome World!");
     }
     public static void printSomething() {
         System.out.println("Print Something");
     }
 
     public static void doSomething() {
-        System.out.println("Do Something");
+        System.out.println("Please Do Something");
     }
 }
