b054349d4bb77faf4ce3565c264cc50523d381d5
dibya <dibya2939@gmail.com>
2026-08-30 22:44:59 +0530

    I have changes hello world method

--- src/main/java/com/nt/App.java
+++ src/main/java/com/nt/App.java
@@ -2,10 +2,10 @@ package com.nt;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
     public static void printSomething() {
-        System.out.println("Print Something");
+        System.out.println("Please Print Something");
     }
 
     public static void doSomething() {
